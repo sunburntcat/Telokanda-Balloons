1daa9161c230e813e5ddcc88fe572cacbb7ff594
sunburntcat <32531379+sunburntcat@users.noreply.github.com>
2020-08-28 20:45:44 -0500

    Create calculate_elevation.js

---
+++ calculate_elevation.js
@@ -0,0 +1,59 @@
+// This code is meant to be used to add a zElevation variable
+//    to the Catena 4610 decoded JSON output
+// It uses the meteorological hypsometric equation to derive
+//    atmospheric height by integrating pressure, dew point, and
+//    temperature at various levels.
+// 
+// For reference:
+//     https://en.wikipedia.org/wiki/Hypsometric_equation
+//
+// Instructions:
+//    Copy and paste this code into a node-red "function" node,
+//      and provide it with Catena 4610 outputs
+
+// Calculate actual vapor pressure
+tDewC = msg.payload.tDewC;
+e = 6.112*Math.pow(10,(7.5*tDewC)/(237.7+tDewC));
+
+// Calculate mixing ratio
+p2 = msg.payload.p;
+w = 0.62197 * ( e / (p2-e));
+
+// Calculate Tv in kelvin
+t2 = msg.payload.tempC + 273.15;
+tv2 = (1+(0.61*w))*t;
+
+// Check if the first observation exists
+if (context.get(msg.devid+'.z1')||0)
+{
+    // Get previous layers' values
+    p1 = context.get(msg.devid+'.p1');
+    tv1 = context.get(msg.devid+'.tv1');
+    z1 = context.get(msg.devid+'.z1');
+    
+    // Approximate average virtual temperature of layer
+    tv = tv1 + (tv2 - tv1)/2.0;
+
+    // Do thickness calculation
+    R = 287.058; // specific gas constant dry air
+    g = 9.80664; // gravitational acceleration
+    z2 = (R*tv/g)*Math.log(p1/p2) + z1;
+
+    // Store values for next iteration
+    context.set(msg.devid+'.p1', p2);
+    context.set(msg.devid+'.tv1', tv2);
+    context.set(msg.devid+'.z1', z2);
+
+    msg.payload.zElevation = z2;
+    return msg;
+    
+} else { // First field doesn't exist
+
+    // Simply store values for next iteration
+    context.set(msg.devid+'.p1', p2);
+    context.set(msg.devid+'.tv1', tv2);
+    context.set(msg.devid+'.z1', 1.0); // 1 meter above ground
+    
+    msg.payload.zElevation = 1.0;
+    return msg;
+}
